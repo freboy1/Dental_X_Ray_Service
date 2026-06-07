61f4a94122343ff970713f142b34185ac1f8551a
Алишер Platform <@a.dautov>
2026-06-08 02:31:00 +0500

    Allow origins

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 from ultralytics import YOLO
 from PIL import Image
 import io
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 model = YOLO("best.pt")
 
